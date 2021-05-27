890880dc27950d6ad44362e703b52fc627bd5656
Renante D. Entera <renante_entera@ligph.com>
2021-05-27 12:58:57 +0800

    Create db seed

---
+++ db/seeds.rb
@@ -0,0 +1,20 @@
+roles = ["teacher", "ta", "student"]
+10.times do
+  course = Course.create(
+    name: Faker::Science.scientist
+  )
+
+  5.times do
+    user = User.create(
+      first_name: Faker::TvShows::RickAndMorty.character,
+      last_name:  Faker::Games::Zelda.character
+    )
+
+    Enrollment.create(
+      role:      roles.sample,
+      course_id: course.id,
+      user_id:   user.id
+    )
+  end
+end
+puts "data seeded"
