6f9e6467f35379f0585d7a6c964b19ad3e4e8fc2
Renante D. Entera <renante_entera@ligph.com>
2021-05-27 17:46:57 +0800

    Add route and update controller actions for enrollments

--- app/controllers/enrollments_controller.rb
+++ app/controllers/enrollments_controller.rb
@@ -1,5 +1,39 @@
 class EnrollmentsController < ApplicationController
-  def index; end
+  before_action :set_course
 
-  def new; end
+  def index
+    # @enrollments = @course.enrollments
+    @teachers = @course.enrollments.where(role: "teacher")
+    @tas = @course.enrollments.where(role: "ta")
+    @students = @course.enrollments.where(role: "student")
+  end
+
+  def new
+    @users = User.all - @course.users
+    @enrollment = @course.enrollments.new
+  end
+
+  def create
+    @enrollment = @course.enrollments.new(enrollment_params)
+    if @enrollment.save
+      redirect_to course_enrollments_path(@course)
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @course.enrollments.find(params[:id]).destroy
+    redirect_to course_enrollments_path(@course)
+  end
+
+  private
+
+  def enrollment_params
+    params.require(:enrollment).permit(:role, :user_id)
+  end
+
+  def set_course
+    @course = Course.find(params[:course_id])
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   root 'courses#index'
 
   resources :users
-  resources :courses
+  resources :courses do
+    resources :enrollments, only: %i[index new create destroy]
+  end
 end
