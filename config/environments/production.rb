66aa43ff373f68782e70593926c5975085de78de
Renante D. Entera <renante_entera@ligph.com>
2021-05-27 15:41:04 +0800

    Change app label

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -60,7 +60,7 @@ Rails.application.configure do
 
   # Use a real queuing backend for Active Job (and separate queues per environment).
   # config.active_job.queue_adapter     = :resque
-  # config.active_job.queue_name_prefix = "manual_ror_app_production"
+  # config.active_job.queue_name_prefix = "ror_lms_sum20_production"
 
   config.action_mailer.perform_caching = false
 
