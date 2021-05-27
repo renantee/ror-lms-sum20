4f67f2d094c99c410add424c5dc547d377968e08
Renante D. Entera <renante_entera@ligph.com>
2021-05-27 15:26:31 +0800

    Create actions to controllers for users and courses

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -1,9 +1,50 @@
 class CoursesController < ApplicationController
-  def index; end
+  before_action :set_course, only: %i[show edit update destroy]
+
+  def index
+    @courses = Course.all
+  end
 
   def show; end
 
-  def new; end
+  def new
+    @course = Course.new
+    render partial: "form"
+  end
 
-  def edit; end
+  def create
+    @course = Course.new(course_params)
+    if @course.save
+      redirect @course
+    else
+      render :new
+    end
+  end
+
+  def edit
+    render partial: "form"
+  end
+
+  def update
+    if @course.update(course_params)
+      redirect_to @course
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @course.destroy
+    redirect_to root_path
+  end
+
+  private
+
+  def course_params
+    params.require(:course).permit(:name)
+  end
+
+  def set_course
+    @course = Course.find(params[:id])
+  end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,50 @@
 class UsersController < ApplicationController
-  def index; end
+  before_action :set_user, only: %i[show edit update destroy]
+
+  def index
+    @users = User.all
+  end
 
   def show; end
 
-  def new; end
+  def new
+    @user = User.new
+    render partial: "form"
+  end
 
-  def edit; end
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to @user
+    else
+      render :new
+    end
+  end
+
+  def edit
+    render partial: "form"
+  end
+
+  def update
+    if @user.update(user_params)
+      redirect_to @user
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @user.destroy
+    redirect_to users_path
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:first_name, :last_name)
+  end
+
+  def set_user
+    @user = User.find(params[:id])
+  end
 end
